fb01b2914ee9bcbe87d6d86a22f999ce45cea604
renejfc <renejfc@gmail.com>
2020-11-30 11:34:11 +0100

    isAdmin middleware added

--- public/js/app.js
+++ public/js/app.js
@@ -19395,8 +19395,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/lorenacriadomanzaneque/Documents/GitHub/f5aeventos/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/lorenacriadomanzaneque/Documents/GitHub/f5aeventos/resources/css/app.css */"./resources/css/app.css");
+__webpack_require__(/*! C:\Users\renej\Documents\xProjects\f5aeventos\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\renej\Documents\xProjects\f5aeventos\resources\css\app.css */"./resources/css/app.css");
 
 
 /***/ })
