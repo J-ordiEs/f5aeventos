8f1676928d7df85dec41d18a81ee2df51b8f5667
Laura Bassani <bassanilaurarita@gmail.com>
2020-11-30 11:50:39 +0100

    test Return Event View

--- tests/Feature/EventsTest.php
+++ tests/Feature/EventsTest.php
@@ -17,29 +17,15 @@ class EventsTest extends TestCase
         $response->assertStatus(200);
     }
 
-        public function testDatabase()
-    {
-        // Make call to application...
-
-        $this->assertDatabaseHas('events', [
-            'id' => 1,
-            // 'date' => '2020-11-16 09:36:19',
-            // 'time'=> '00:00:00',
-            'title' => 'Css',
-            'speaker' => 'steve jobs',
-            'image' => 'imagen',
-            'description' => 'css is cool',
-            'capacity' => 20,
-            'requirements' => 'mic',
-            'zoom' => 'www.pages.com',
-            'special' => false,
-            'past' => false
-        ]);
-    }
-
     public function testCreateEvent()
     {
         $events = Event::factory(2)->create();
         $this->assertCount(2, $events);
+    }
+
+    public function testReturnEventView()
+    {
+        $response = $this->get('/events');
+        $response->assertViewIs('events.events');
     }
 }
