e1a3560a3f66dcbc8aa6b292a65577045dbbb146
renejfc <renejfc@gmail.com>
2020-11-30 12:46:31 +0100

    feature progress

---
+++ resources/views/layouts/card.blade.php
@@ -0,0 +1,12 @@
+<article>
+  <header>
+      <h3>Titulo</h3>
+      <h5>Fecha-Hora</h5>
+  </header>
+  <figure>
+  <img src="asd" alt="cardimg">
+  <figcaption>
+    <p>10/100</p>
+  </figcaption>
+  </figure>
+</article>
---
+++ resources/views/layouts/eventInfo.blade.php
@@ -0,0 +1,26 @@
+<main>
+  <section>
+    <header>
+      <h1>Titulo</h1>
+      <h2>Fecha/Hora</h2>
+    </header>
+    <figure>
+      <img src="" alt="">
+      <h2>Descripción</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi illum quod nisi nobis consectetur ut saepe pariatur consequatur beatae neque. Eaque numquam deleniti consequatur omnis quaerat eius dolorum exercitationem adipisci!</p>
+      <h3>Ponente</h3>
+    </figure>
+  </section>
+  <section>
+    <div>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+    <h3>Plazas Disponibles</h3>
+    <h2>10/100</h2>
+    <button>Inscribirme</button>
+  </section>
+</main>
